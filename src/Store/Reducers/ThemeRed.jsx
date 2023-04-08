fcf341bae6f28d0b20549c77e7f371136cd8236c
yash <ymandlekar9826@gmail.com>
2023-04-08 11:45:50 +0530

    updating

--- src/Store/Reducers/ThemeRed.jsx
+++ src/Store/Reducers/ThemeRed.jsx
@@ -1,11 +1,14 @@
 import { createReducer } from "@reduxjs/toolkit";
 
+const theme = localStorage.getItem("theme") || "dark";
+
 const initialState = {
-  theme: "light",
+  theme: theme,
 };
 
 export const themeReducer = createReducer(initialState, {
   changeThemeSuccess: (state, action) => {
     state.theme = action.payload;
+    localStorage.setItem("theme", action.payload);
   },
 });
